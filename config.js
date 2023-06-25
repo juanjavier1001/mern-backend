e92d615f25e30fd01acf79886b638933f9863972
juanjavier1001 <javierdeveloper1001@gmail.com>
2023-06-25 04:10:06 -0300

    corrigo db_name todo mayuscula

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ export const PORT = process.env.PORT || 4000;
 const DB_HOST = process.env.DB_HOST || 'localhost';
 const DB_USER = process.env.DB_USER || 'root';
 const DB_PASSWORD = process.env.DB_PASSWORD || '';
-const DB_Name = process.env.DB_Name || 'taskpb2023';
+const DB_NAME = process.env.DB_NAME || 'taskpb2023';
 const DB_PORT = process.env.DB_PORT || '';
 
-export { DB_HOST, DB_USER, DB_PASSWORD, DB_PORT, DB_DATABASE };
+export { DB_HOST, DB_USER, DB_PASSWORD, DB_PORT, DB_NAME };
--- db.js
+++ db.js
@@ -1,12 +1,6 @@
 import mysql2 from 'mysql2';
 import { createPool } from 'mysql2/promise';
-import {
-	DB_DATABASE,
-	DB_HOST,
-	DB_PASSWORD,
-	DB_PORT,
-	DB_USER,
-} from './config.js';
+import { DB_NAME, DB_HOST, DB_PASSWORD, DB_PORT, DB_USER } from './config.js';
 
 const pool = createPool({
 	host: DB_HOST,
@@ -15,9 +9,8 @@ const pool = createPool({
 
 	password: DB_PASSWORD,
 
+	database: DB_NAME,
 	port: DB_PORT,
-
-	database: DB_DATABASE,
 });
 
 //exportamos la conexion a la db
